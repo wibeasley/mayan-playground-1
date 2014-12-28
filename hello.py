502b5c3c0e2b6e5e4e5f9fd520b41dd2e8f34fff
mmbeasley <peterpankidsmw@gmail.com>
2014-12-27 18:01:58 -0600

    learning variable/labels

--- hello.py
+++ hello.py
@@ -2,3 +2,18 @@ print("Hello World")
 print(765+789)
 print(678+654)
 print(6655+5784)
+
+fred=100
+print(fred)
+print("fred")
+
+fred=200
+print(fred)
+john=fred
+print("john")
+
+number_of_coins=200
+print(number_of_coins)
+print("number_of_coins: " + str(number_of_coins))
+
+
