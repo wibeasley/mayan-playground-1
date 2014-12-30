a42f9a846ae420c1b6c2b893b0a9e205349f7fa8
mmbeasley <peterpankidsmw@gmail.com>
2014-12-30 10:33:48 -0600

    Initial work w/ Teagen

---
+++ teagen/chapter_01/hello.py
@@ -0,0 +1,12 @@
+print("hello world")
+print("hello world")
+"hello world-c"
+
+
+
+print("I am Teagen!-hi")
+"I am Teagen!"
+print("I am Teagen")
+"I am Teagen!"
+print("Bye-bye")
+"bye bye"
---
+++ teagen/chapter_02/calculations_and_variables.py
@@ -0,0 +1,14 @@
+fred =100
+print(fred)
+print(fred)
+
+fred = 200
+print(fred)
+print(fred)
+
+number_of_coins = 200
+
+john = fred
+fred = john
+adam = "jj" 
+print(adam)
