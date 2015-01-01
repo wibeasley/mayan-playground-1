7478269ef21d32ee6ef389776c2c97aea408c35d
mmbeasley <peterpankidsmw@gmail.com>
2015-01-01 16:48:18 -0600

    Finishing strings section'

--- chapter_03/strings_lists_tuples_and_maps.py
+++ chapter_03/strings_lists_tuples_and_maps.py
@@ -3,15 +3,13 @@
 fred = '1GUB875T776R56798O987T787T598785TIJUHGTFCYGJHKHGFCG'
 print(fred)
 
-fred = "rrrrr" 
+fred = "rrrrr"
 fred = '''2GUB875T776R56798O987Tmfkuyj
 7T598785TIJUHGTFCYGJHKHGFCG'''
-
-
-print(fred)
-
-
-##;oiuguio;oiugbyhjhvgrsqASFGFRETYJIIII87TGU'
+#'''goVOGOGOI87TOIUVP/;G3B8/bo3V0GB8V098 bh"pOIBg{
+##-HN;H;OBH;Bh'BBGOI9ogbgPO9hbpBI'''
+#qqqwwweeerrrtttyyyuuuiiiooopppaaasssdddfffggghhhjjjkkklllzzzxxxcccvvvbbbnnnmmm
+## prioiuguio;oiugbyhjhvgrsqASFGFRETYJIIII87TGU'
 ##GUB875T776R56798O987T787T598785TIJUHGTFCYGJHKHGFCG")             
 
 silly_string = 'He said, "Aren'
@@ -21,3 +19,43 @@ silly_string = '''He said, "Aren't can't shouldn't wouldn't"'''
 
 silly_string = 'He said, "Aren\'t can\'t shouldn\'t wouldn\'t"'
 
+myscore = 1000
+message = 'I scored %s points'
+print(message % myscore)
+print('I scored %s points' % 1003)
+print('I scored %s points', 1004)
+
+
+print(message)
+joke_text = '%s: a device for finding furniture in the dark'
+bodypart_1 = "Elbow"
+bodypart_2 = "butt"
+print(joke_text % bodypart_1)
+print(joke_text % bodypart_2)
+
+nums = 'What did then number %s say to the number %s? Nice belt!!'
+print(nums % (0, 8))
+
+message = 'I scord 1000 points'
+message = 'I scord 2 points'
+message = 'I scord 3 points'
+message = 'I scord 4 points'
+message = 'I scord 1000 points'
+message = 'I scord 2 points'
+message = 'I scord 3 points'
+
+print(10 * 'a')
+spaces = ' ' * 25
+print('%s 12 Butts Wynd' % spaces)
+print('%s Twinklebottom Health' % spaces)
+print('%s West Snoring' % spaces)
+print()
+print()
+print('Dear Sir')
+print()
+print('I wish to report tiles are missing from the')
+print('outside toilet roof.')
+print('I think it was that windy of a night that blew them away.')
+print()
+print('Regards')
+print('Malcolm Dithering')
