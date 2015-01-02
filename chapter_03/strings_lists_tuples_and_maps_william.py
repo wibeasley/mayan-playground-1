863a14e645212c0e07bd1787f65d391dd1468b4c
Will Beasley <wibeasley@hotmail.com>
2015-01-01 19:13:01 -0600

    William's adventure's with Ella

---
+++ chapter_03/strings_lists_tuples_and_maps_william.py
@@ -0,0 +1,24 @@
+#This involves a witch changing Ella
+#into different animals
+
+ella = 'elephant'
+print(ella)
+
+ella = "another witch" 
+print(ella)
+
+
+ella = '''goblin'''
+print(ella)
+
+ella = "ella is really magic"
+print(ella)
+
+ella = "a mean mean witch"
+print(ella)
+
+ella = "too so we"
+print(ella)
+
+ella = "ellasowe"
+print(ella)
