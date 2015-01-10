f8a4b4fc49e5c7040d3b81a12d9c5a16ee85d4e6
mmbeasley <peterpankidsmw@gmail.com>
2015-01-10 12:20:55 -0600

    adding items to a list

--- chapter_03/lists.py
+++ chapter_03/lists.py
@@ -27,15 +27,43 @@ some_numbers = [1,2,5,10,20]
 print(some_numbers)
 some_strings = ['which',  'which', 'is', 'which']
 print(some_strings)
-type(some_strings)
 
 numbers_and_string = [3, 4, 'which']
 print(numbers_and_string)
 
 super_list = [wizard_list, some_numbers, some_strings]
 print(super_list)
-
 print(6 + 1)
 print("6" + "1")
 #Throws error: "6" + 1
 print(46 + 49)
+#qqq www eee rrr ttt yyy uuu iii ooo ppp aaa sss ddd fff ggg hhh jjj
+#kkk lll zzz xxx ccc vvv bbb nnn mmm QQQ WWW EEE RRR TTT YYY UUU III
+# OOO PPP AAA SSS DDD FFF GGG HHH JJJ KKK LLL ZZZ XXX CCC VVV BBB NNN
+#MMM ``` 111 222 333 444 555 666 777 888 999 000 --- === [[[ ]]] ;;;
+#''' ,,, ... /// \\\ ~~~ !!! @@@ ### $$$ %%% ^^^ &&& *** ((( ))) ___ +
+#++ {{{ }}} ||| ::: """ <<< >>> ??? 
+
+numbers = [1, 2, 3, 4]
+print(numbers)
+strings = ['I', 'kicked', 'my', 'toe', 'and', 'it', 'is', 'sore']
+print(strings)
+mylist = [numbers, strings]
+print(mylist)
+
+############### Adding items to a list ############### 
+wizard_list.append('bear burp')
+print(wizard_list)
+print(wizard_list)
+wizard_list.append('swanp gas')
+wizard_list.append('hemlock')
+wizard_list.append('mandrake')
+print(wizard_list)
+ 
+
+
+
+
+
+
+
