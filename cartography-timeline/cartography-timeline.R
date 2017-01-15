9686c5b6bc0acb31260b60c3614a80adca4c41b7
Will Beasley <wibeasley@hotmail.com>
2017-01-15 14:40:39 -0600

    knitr report for timeline

--- cartography-timeline/cartography-timeline.R
+++ cartography-timeline/cartography-timeline.R
@@ -12,6 +12,7 @@ requireNamespace("knitr")
 requireNamespace("scales") #For formating values in graphs
 requireNamespace("RColorBrewer")
 requireNamespace("dplyr")
+requireNamespace("ggthemes")
 # requireNamespace("tidyr") #For converting wide to long
 # requireNamespace("mgcv, quietly=TRUE) #For the Generalized Additive Model that smooths the longitudinal graphs.
 # requireNamespace("TabularManifest") # devtools::install_github("Melinae/TabularManifest")
@@ -22,22 +23,22 @@ options(show.signif.stars=F) #Turn off the annotations on p-values
 # path_input <- "./data-phi-free/derived/motor-trend-car-test.rds"
 data_string <- '
 date,milestone,position_y
-6700 BCE,"map of Catal Hyuk - earliest known map",5
-3000 BCE,"Babylonian clay tablets including map of present day Iraq",10
-3000 BCE,"first map of the moon",15
-600 BCE,"Pythagoas determines earth is a sphere",20
+6700 BCE,"map of Catal Hyuk - earliest known map",4
+3000 BCE,"Babylonian clay tablets including map of present day Iraq",8
+3000 BCE,"first map of the moon",13
+600 BCE,"Pythagoas determines earth is a sphere",18
 200 BCE,"Eratosthenes determines circumpherence of the earth",-5
-150 BCE,"Hipparchus creates early system of latitude based on the sun",-10
-150 CE,"Ptolemy creates world map with coordinates for 8,000 locations",-15
-476 CE,"Roman Empire collapses, scientific cartography halts in Europe",-20
+150 BCE,"Hipparchus creates early system of latitude based on the sun",-12
+150 CE,"Ptolemy creates world map with coordinates for 8,000 locations",-3
+476 CE,"Roman Empire collapses, scientific cartography halts in Europe",-18
 830 CE,"Al-Khwarizmi produces wowrld map partly based on Ptolemy\'s work",-20
 1030 CE,"Al-Biruni develops triangulation",20
-1375 CE,"Abraham Cresques produces world map using portolan maps",-15
+1375 CE,"Abraham Cresques produces world map using portolan maps",2
 1569 CE,"Gerardus Mercator publishes his first projection for navigation",15
 1730 CE,"sextant developed",0
 1763 CE,"John Harrison develops acurate clock for determining longitude at sea",5
 1802 CE,"Nathaniel Bowditch\'s The New American Practical Navigator is first published",10
-1884 CE,"International Meridian Conference chooses Greenwich as prime meridian",-10
+1884 CE,"International Meridian Conference chooses Greenwich as prime meridian",-8
 1990 CE,"high-quality maps produced from aerial and satellite imagery and GIS and GPS developed",-15
 2000 CE,"the space shuttle Endeavour maps more then 80 percent of the earth\'s surface in 10 days",-10
 '
@@ -51,13 +52,18 @@ ds <- ds %>%
   dplyr::mutate(
     is_bc       = (gsub(pattern, "\\2", date) == "BCE"),
     date        = as.integer(gsub(pattern, "\\1", date)),
-    date        = ifelse(is_bc, -date, date)
+    date        = ifelse(is_bc, -date, date),
+    y2          = order(-date)
   )
 
 
-# ---- graph -------------------------------------------------------------------
+# ---- graph-continuous -------------------------------------------------------------------
 
-ggplot(ds, aes(x=date, y=position_y, label=milestone))  +
-  geom_point()
+ggplot(ds, aes(x=date, y=y2, label=milestone))  +
+  geom_point() +
+  geom_text(hjust=0) +
+  scale_x_continuous(breaks=c(-7000,  2000)) +
+  coord_cartesian(xlim=c(-6800, 200000)) #+
+  # ggthemes::theme_solarized()
 
 
---
+++ cartography-timeline/cartography-timeline.md
@@ -0,0 +1,66 @@
+# Cartography Timeline
+Date: `r Sys.Date()`  
+
+This report covers the analyses used in the ZZZ project (Marcus Mark, PI).
+
+<!--  Set the working directory to the repository's base directory; this assumes the report is nested inside of two directories.-->
+
+
+<!-- Set the report-wide options, and point to the external code file. -->
+
+
+<!-- Load 'sourced' R files.  Suppress the output when loading sources. --> 
+
+
+<!-- Load packages, or at least verify they're available on the local machine.  Suppress the output when loading packages. --> 
+
+
+<!-- Load any global functions and variables declared in the R file.  Suppress the output. --> 
+
+
+<!-- Declare any global functions specific to a Rmd output.  Suppress the output. --> 
+
+
+<!-- Load the datasets.   -->
+
+
+<!-- Tweak the datasets.   -->
+
+
+# Summary
+
+# Graph on Continuous Time
+![](figure-png/graph-continuous-1.png)<!-- -->
+
+# Session Information
+For the sake of documentation and reproducibility, the current report was rendered on a system using the following software.
+
+```
+Report rendered by wibeasley at 2017-01-15, 14:40 -0600 in 1 seconds.
+```
+
+```
+R version 3.3.1 (2016-06-21)
+Platform: x86_64-pc-linux-gnu (64-bit)
+Running under: Ubuntu 16.04.1 LTS
+
+locale:
+ [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8       
+ [4] LC_COLLATE=en_US.UTF-8     LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
+ [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                  LC_ADDRESS=C              
+[10] LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+[1] magrittr_1.5  ggplot2_2.2.1 knitr_1.15.1 
+
+loaded via a namespace (and not attached):
+ [1] Rcpp_0.12.9        munsell_0.4.3      colorspace_1.3-2   R6_2.2.0           stringr_1.1.0     
+ [6] plyr_1.8.4         dplyr_0.5.0.9000   tools_3.3.1        grid_3.3.1         gtable_0.2.0      
+[11] DBI_0.5-1          ggthemes_3.3.0     htmltools_0.3.5    yaml_2.1.14        lazyeval_0.2.0    
+[16] rprojroot_1.1      digest_0.6.11      assertthat_0.1     tibble_1.2         readr_1.0.0       
+[21] RColorBrewer_1.1-2 evaluate_0.10      rmarkdown_1.3      labeling_0.3       stringi_1.1.2     
+[26] scales_0.4.1       backports_1.0.4   
+```
